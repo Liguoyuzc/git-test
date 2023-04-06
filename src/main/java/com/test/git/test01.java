b66b15aec7739345d8f52caca155ba03cfb0261e
Liguoyuzc <130026395+Liguoyuzc@users.noreply.github.com>
2023-04-06 21:40:39 +0800

    test02

--- src/main/java/com/test/git/test01.java
+++ src/main/java/com/test/git/test01.java
@@ -10,5 +10,6 @@ public class test01 {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("test01");
+        System.out.println("test02");
     }
 }
